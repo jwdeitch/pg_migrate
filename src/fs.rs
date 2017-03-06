033c8fe1613c6789f0625a6cb42549f613b563d6
jwdeitch <jwdeitch@gmail.com>
2017-03-06 00:57:48 -0500

    processing forward and backward fs walk

--- src/fs.rs
+++ src/fs.rs
@@ -1,23 +1,28 @@
 extern crate walkdir;
 
-pub fn walk(dir: String) {
+pub fn walk(dir: String, is_up: bool) {
     for entry in walkdir::WalkDir::new(dir) {
         let entry = entry.unwrap();
 
-        if should_skip(&entry) {
+        if should_skip(&entry, &is_up) {
             continue;
         }
 
-
         println!("{}", entry.path().display());
     }
-
 }
 
-fn should_skip(entry: &walkdir::DirEntry) -> bool {
+fn should_skip(entry: &walkdir::DirEntry, is_up: &bool) -> bool {
     entry.file_name()
         .to_str()
-        .map(|s| s.starts_with(".") || s.ends_with(".sql"))
+        .map(|s| {
+            if *is_up && s.ends_with("-up.sql") {
+                false
+            } else if !*is_up && s.ends_with("-down.sql") {
+                false
+            } else {
+                true
+            }
+        })
         .unwrap_or(false)
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
 mod fs;
 
+extern crate clap;
+
+use clap::{App, SubCommand};
+
 fn main() {
-    fs::walk(".".to_string());
-    println!("Hello, world!");
+    let matches = App::new("Postgres Schema Migrator")
+        .version("0.1")
+        .author("https://github.com/jwdeitch/pg_migrate")
+        .subcommand(SubCommand::with_name("up")
+            .about("forward migration, files postfixed with -up"))
+        .subcommand(SubCommand::with_name("down")
+            .about("rollback migrations, files postfixed with -down"))
+        .subcommand(SubCommand::with_name("status")
+            .about("Retrieve migration Status"))
+        .subcommand(SubCommand::with_name("configure")
+            .about("Setup migrations table"))
+        .get_matches();
+
+    if matches.is_present("status") {
+        println!("STATUS IS PRESENT");
+    }
+
+    if matches.is_present("up") {
+        fs::walk(".".to_string(), true);
+    }
+
+    if matches.is_present("down") {
+        fs::walk(".".to_string(), false);
+    }
+
+    if matches.is_present("configure") {
+        println!("CONFIGURE");
+    }
 }
-
-
-
-
-/*
-
-folder1
-   |-----------f2
-                |------------
-
-*/
